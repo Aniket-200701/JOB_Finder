75af9e14528f9554574f23a158ff25903b2b43a4
Aniket Kumar <aniketsingh0407@gmail.com>
2024-07-01 12:29:35 +0530

    ionicproject

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JOB_Finder_App',
+  webDir: 'www'
+};
+
+export default config;
